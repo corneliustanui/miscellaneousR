2429edbaf2a017cf1f938b71ed43b1f858bb6c75
Cornelius Tanui <kiplimocornelius@gmail.com>
2023-03-29 20:06:55 +0300

    Updated package

--- R/mean1.R
+++ R/mean1.R
@@ -1,3 +1,14 @@
+
+#' mean1
+#'
+#' @param x a numeric vector for which to compute mean
+#'
+#' @return a numeric value
+#' @export
+#'
+#' @examples
+#' x <- 1:20
+#' mean1(x)
 mean1 <- function(x){
   if(is.numeric(x)){
     return(mean(x))
